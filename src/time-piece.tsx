d44a4cd3980a85d6cdacd87271c782871cc04051
Matt Cordier <mcordier24@gmail.com>
2023-09-17 12:41:36 -1000

    update stopwatch button style and conditions

--- src/time-piece.tsx
+++ src/time-piece.tsx
@@ -1,19 +1,23 @@
 import useClock from "./hooks/useClock";
 import useStopWatch from "./hooks/useStopWatch";
 import { TimePieceCanvas } from "./time-piece-canvas";
+import styles from "./time-piece.module.css"
 
 export function TimePiece(props: any) {
 
     const { mode } = props
 
     const { seconds, minutes, hours } = useClock()
-    const { tick, handleStart, handlePause, handleReset } = useStopWatch()
+    const { tick, handleStart, handlePause, handleReset, isActive, isPaused } = useStopWatch()
 
     return (
-        <TimePieceCanvas mode={mode} ss={seconds} mm={minutes} hh={hours} tick={tick} size={600}>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handlePause}>Pause</button>
-            <button onClick={handleReset}>Reset</button>
-        </TimePieceCanvas>
+        <TimePieceCanvas mode={mode} size={600} ss={seconds} mm={minutes} hh={hours} tick={tick} >
+            {(!isActive ?
+                <button onClick={handleStart} className={styles.startBtn} >Start</button> :
+                <button onClick={handlePause} className={styles.pauseBtn}>Pause</button>
+            )
+            }
+            < button onClick={handleReset} className={styles.resetBtn}>Reset</button>
+        </TimePieceCanvas >
     )
 }
